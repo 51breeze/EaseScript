3015a284c2a193fcd053a9b1d0ed01f94eecae05
Administrator <664371281@qq.com>
2017-09-23 22:49:17 +0800

    1.0.6

---
+++ ColorThemes.js
@@ -0,0 +1,25 @@
+/**
+* Created by Administrator on 2016/1/20.
+*/
+module.exports = {
+
+    'default':{
+        //场景颜色
+        'scene':'#ffffff',
+        //强调颜色
+        'highlight':'#353535',
+        //主要的颜色
+        'primary':'#353535',
+        //辅助的颜色
+        'secondary':'#DFDFDF', //#DFDFDF //#337ab7
+        //标题文本颜色
+        'normal':'#333333',
+        //成功颜色
+        'success':'#5cb85c',
+        //警告颜色
+        'warn':'#f0ad4e',
+        //错误颜色
+        'error':'#d9534f',
+    }
+
+};
