0bb018a344d1e158b0ea04b35351da33fa339096
Jun Ye <664371281@qq.com>
2019-10-01 11:40:11 +0800

    test

---
+++ test/main.js
@@ -0,0 +1,50 @@
+
+import "./style/less/main.less";
+import Index from './test/src/IndexApplication.es';
+import Event from '@system/Event.js';
+import System from '@system/System.js';
+
+// require("./es/skins/DataGridStyle.less");
+// require("./es/skins/NavigateStyle.less");
+// require("./es/skins/PaginationStyle.less");
+// require("./es/skins/PopUpStyle.less");
+
+
+
+
+var obj = new Index();
+var global = System.getGlobalEvent();
+global.addEventListener(Event.READY,function (e) {
+
+   // import(/*webpackChunkName:"index2"*/ './test/src/IndexApplication.es' )
+
+   // import(/*webpackChunkName:"person"*/ './test/src/PersonApplication.es' ).then(function(module){
+
+        //console.log( module.default );
+
+        //console.log("==========");
+
+      // var obj = new module.default();
+      // obj.index();
+
+    //});
+   
+    console.log(  obj.home()+""  );
+
+},false,-500);
+
+if( module.hot ){
+
+    module.hot.accept('./test/src/IndexApplication.es', function() {
+         
+
+        //var obj = new Index();
+        // obj.home();
+        //window.location.reload();
+        //console.log("./test/src/IndexApplication.es===============")
+
+    });
+
+
+}
+
---
+++ test/test.js
@@ -0,0 +1,267 @@
+const path = require("path");
+const webpack = require("webpack");
+const es = require("./lib/index");
+const builder = require("./javascript/builder");
+const fs = require("fs");
+const webpackDevServer = require('webpack-dev-server');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const esConfig = es.getConfigure(  JSON.parse( fs.readFileSync( path.resolve(__dirname, 'test/.esconfig') ) ) );
+
+const basedir = path.resolve("d:/workspace/EaseScript");
+loader = path.resolve(basedir,'lib/loader.js');
+
+const lessOptions = {
+  globalVars:builder.getLessVariables( esConfig ),
+  paths:[
+      path.resolve(__dirname,'style'),
+  ]
+}
+
+const config = {
+  mode:"development",
+  //mode:"production",
+  devtool:"(none)",
+  entry:{
+    'index': path.resolve(__dirname,'main.js'),
+   // 'person': path.resolve(__dirname,'main2.js'),
+  //  'test':'./test/src/Test.es'
+    //'address':'./test/src/IndexApplication.es?main',
+  },
+  output: {
+    path:path.resolve('./test/build'),
+    filename: './js/[name].bundle.js',
+    chunkFilename:'./js/[name].bundle.js',
+  },
+  resolve:{
+    extensions:[".js", ".json", ".jsx", ".css",'.es'],
+    alias:{
+      "@system":path.resolve(__dirname, "javascript/system"),
+      "@core":path.resolve(__dirname),
+      "@src":path.resolve(__dirname),
+      "@style":path.resolve(__dirname)
+    },
+    modules:[
+     path.resolve(__dirname),
+     path.resolve(__dirname,"test"),
+     path.resolve(__dirname, "javascript"),
+     path.resolve(__dirname, "javascript/system"),
+     path.resolve(__dirname, "node_modules"),
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve('./test/build'),
+    hot:true,
+    //inline:true,
+    host:'localhost',
+    open:false
+  },
+  watch:true,
+  // watchOptions:{
+  //     ignored: /node_modules/
+  // },
+  module: {
+    rules: [
+      {
+        test: /(\.es)|(javascript\\system\\[a-zA-Z]+\.js)|(\.html)$/,
+        include:[
+           path.resolve(__dirname, "javascript"),
+           path.resolve(__dirname, "es"),
+           path.resolve(__dirname, "test/src"),
+           path.resolve(__dirname, "src"),
+        ],
+        use: [
+          {
+            loader:loader,
+            options:{
+              //config:esConfig,
+              //"styleLoader":[],
+              //  styleLoader:[
+              //    MiniCssExtractPlugin.loader.replace(/\\/g,'/'),
+              //   'css-loader',
+              //   'less-loader'
+              //  ],
+              globalVars:lessOptions.globalVars,
+              paths:lessOptions.paths
+            },
+          }
+        ]
+      },
+      {
+        test:/\.(less|css)$/i,
+        use: [ 
+          //MiniCssExtractPlugin.loader,
+          'style-loader',
+          {
+            loader:'css-loader',
+            options:{
+              onlyLocals:false
+            }
+          },
+          {
+            loader:'less-loader',
+            options:lessOptions
+          }
+        ],
+      },
+      
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|jpeg|gif)$/,
+        use: ['file-loader'],
+      },
+    ]
+  },
+  plugins: [
+   // new MyPlugin({context:__dirname}),
+   // new webpack.MemoryOutputFileSystem()
+   // new ExtractTextWebpackPlugin({filename:'[name].min.css'})
+   //new MiniCssExtractPlugin({filename: "./css/[name].css"}),
+
+    //new webpack.NamedModulesPlugin(),
+    //new webpack.HotModuleReplacementPlugin()
+  ],
+
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 30000,
+      maxSize: 0,
+      minChunks:1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          name:"vendor"
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+          name:"common"
+        }
+      }
+    },
+    runtimeChunk: {
+      name: 'runtime'
+    }
+  }
+
+};
+
+
+const NormalModule = require("webpack/lib/NormalModule");
+const RawModule = require("webpack/lib//RawModule");
+//const RuleSet = require("webpack/lib//RuleSet");
+
+
+
+function MyPlugin( options ) 
+{
+    this.options = Object.assign({suffix:".es"},options||{});
+    if( !this.options.context )
+    {
+       this.options.context= path.dirname( path.dirname( require.resolve('easescript') ) );
+    }
+};
+
+
+MyPlugin.prototype.apply = function(compiler) 
+{
+
+  // compiler.hooks.done.tapAsync("MyPluginDone",function(compiler, callback){
+
+  //         //console.log( compiler.compilation )
+  //       callback();
+  // })
+
+  var options = this.options;
+
+  console.log( "=============MyPlugin==============" )
+
+
+  compiler.hooks.make.tapAsync('MyPluginMake',function(compilation, callback ){
+
+      compilation.hooks.finishModules.tapAsync("MyPluginMake",function(modules,callback){
+            //console.log(  modules );
+            callback();
+      });
+
+      compilation.hooks.succeedEntry.tap("MyPluginMake",function(entry, name, module){
+            //console.log(  entry, name );
+            //callback();
+      });
+
+      // compilation.hooks.buildModule.tap("buildModule",function(module,callback){
+      //      // console.log( module );
+      //       //callback();
+      // });
+
+      console.log( "=============finishModules==============" )
+
+      callback();
+
+
+  });
+
+  compiler.hooks.beforeRun.tapAsync("MyPlugin",function(compiler, callback){
+
+
+
+  //  console.log("This is an example plugin!!!", compiler.options );
+    // 功能完成后调用 webpack 提供的回调。
+    callback();
+
+
+  })
+
+ 
+  // compiler.plugin('run', function(compilation, callback)
+  // {
+  //    console.log("This is an example plugin!!!");
+  //    // 功能完成后调用 webpack 提供的回调。
+  //    callback();
+  // });
+
+
+    compiler.hooks.thisCompilation.tap("MyPlugin", compilation => {
+    
+
+        compilation.hooks.optimizeChunksAdvanced.tap("SplitChunksPlugin",chunks => {
+
+            //console.log( chunks )
+
+
+        });
+
+    });
+
+};
+
+
+
+
+webpackDevServer.addDevServerEntrypoints(config, config.devServer);
+
+var compiler = webpack( config );
+
+// compiler.run(function(){
+
+//       console.log( "==ok==="  )
+
+// })
+
+const server = new webpackDevServer(compiler, config.devServer);
+
+
+
+server.listen(8080, 'localhost', () => {
+
+   console.log('dev server listening on port 8080');
+
+});
+
+
