3fc9a427bb56ea12ddcd0ae2c948ee6eb76413e1
Jun Ye <664371281@qq.com>
2019-10-01 09:43:16 +0800

    test

---
+++ test/main2.js
@@ -0,0 +1,16 @@
+import "./style/less/main.less";
+import Index from './test/src/PersonApplication.es';
+import Event from 'system/Event.js';
+import System from 'system/System.js';
+
+
+var obj = new Index();
+var global = System.getGlobalEvent();
+global.addEventListener(Event.READY,function (e) {
+   
+    console.log(  obj.index()+""  );
+
+},false,-500);
+
+
+console.log("==PersonApplication===66666")
